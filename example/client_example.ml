9c36ed523f9434e8b8e57ee6f80e9b666f0f3e55
Anurag Soni <anurag@sonianurag.com>
2019-03-04 20:29:05 -0500

    initial commit

---
+++ example/client_example.ml
@@ -0,0 +1,16 @@
+open Core
+open Async
+open Fetch
+
+let main port host () =
+  let u = Uri.with_port (Uri.of_string host) (Some port) in
+  Request.create `GET u >>| fun resp -> print_endline resp
+
+let () =
+  Command.async_spec ~summary:"Start a hello world Async client"
+    Command.Spec.(
+      empty
+      +> flag "-p" (optional_with_default 80 int) ~doc:"int destination port"
+      +> flag "-h" (required string) ~doc:"string destination host")
+    main
+  |> Command.run
